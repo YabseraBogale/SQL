0af8e2d75f26c00a6aa932af3b0a4ebddd19f327
Yabsera Bogale Abate <96526984+YabseraBogale@users.noreply.github.com>
2023-03-02 18:48:22 +0300

    Add files via upload

---
+++ dbstreamlit.sql
@@ -0,0 +1,10 @@
+create table userInformation(
+
+	userName varchar(30) not null primary key
+	firstName varchar(30) not null,
+	middleName varchar(30) not null,
+	lastName varchar(30) not null,
+	password text not null
+	
+
+);
